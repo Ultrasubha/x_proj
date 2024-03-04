53bb86352540fbee4bc13c339d77731415c91c0f
Subhadeep Mandal <ultrasubha@gmail.com>
2024-03-05 02:07:41 +0530

    changes so far

--- x_proj/my_static_files/home.js
+++ x_proj/my_static_files/home.js
@@ -1,11 +1,20 @@
 function modifyUrl() {
-  var currentUrl = window.location.href;
-  var urlArr = currentUrl.split("/");
-  var section = urlArr[urlArr.length - 2];
+  let sectionRoutes = ["#tweets", "#Explore","#Notifications","#Messages",
+  "#Grok","#Lists","#Communities","#Premium","#Profile","#More","#Logout"];
+  let currentUrl = window.location.href;
+  console.log(currentUrl);
+  let urlArr = currentUrl.split("/");
+  let section = urlArr[urlArr.length - 2];
+  let trailing = urlArr[urlArr.length - 1];
 
-  if (section === "home") {
+  if (section === "home" && trailing=="") {
     let modifiedUrl = currentUrl + "#tweets";
-    console.log(modifiedUrl);
     location.href=modifiedUrl;
   }
 }
+
+// if(trailing[0]=="#"){
+//   if(!sectionRoutes.includes(trailing)){
+//     location.href = "http://localhost:8000/home/#tweets"
+//   } 
+// }
