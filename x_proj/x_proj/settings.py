5335903515ece30afa13794a245890f856668f1a
1.subhadeepmandal <1.subhadeepmandal@gmail.com>
2024-03-04 11:50:29 +0530

    .

--- x_proj/x_proj/settings.py
+++ x_proj/x_proj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure--@^ntmilg^wirt4oomjr1@qn9_!64bb5b(f9k0pratdf51*w16
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.110.30.155', 'localhost', '127.0.0.1']
 
 
 # Application definition
