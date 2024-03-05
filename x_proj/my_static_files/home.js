0f73db91db69b7a83f560d24a24fa7dcdb5c3f42
Subhadeep Mandal <ultrasubha@gmail.com>
2024-03-06 01:25:28 +0530

    minor changes

--- x_proj/my_static_files/home.js
+++ x_proj/my_static_files/home.js
@@ -1,20 +1,23 @@
 function modifyUrl() {
-  let sectionRoutes = ["#tweets", "#Explore","#Notifications","#Messages",
-  "#Grok","#Lists","#Communities","#Premium","#Profile","#More","#Logout"];
+  let sectionRoutes = [
+    "#tweets",
+    "#Explore",
+    "#Notifications",
+    "#Messages",
+    "#Grok",
+    "#Lists",
+    "#Communities",
+    "#Premium",
+    "#Profile",
+    "#More",
+    "#Logout",
+  ];
   let currentUrl = window.location.href;
-  console.log(currentUrl);
   let urlArr = currentUrl.split("/");
   let section = urlArr[urlArr.length - 2];
   let trailing = urlArr[urlArr.length - 1];
 
-  if (section === "home" && trailing=="") {
-    let modifiedUrl = currentUrl + "#tweets";
-    location.href=modifiedUrl;
+  if (((trailing[0] == "#") && (!sectionRoutes.includes(trailing))) || (section === "home" && trailing == "") ) {
+    location.href = "http://localhost:8000/home/#tweets"
   }
 }
-//http://localhost:8000/home/#tweets
-// if(trailing[0]=="#"){
-//   if(!sectionRoutes.includes(trailing)){
-//     location.href = "http://localhost:8000/home/#tweets"
-//   } 
-// }
