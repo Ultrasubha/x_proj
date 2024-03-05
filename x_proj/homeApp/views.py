b0be612d5f8c23b2aa54a571a84386b23c512c1f
1.subhadeepmandal <1.subhadeepmandal@gmail.com>
2024-03-05 19:13:25 +0530

    like feature working

--- x_proj/homeApp/views.py
+++ x_proj/homeApp/views.py
@@ -9,8 +9,10 @@ from django.http import HttpResponse
 # @login_required
 def home_view(request):
     user = request.user
-    posts = Post.objects.values('author__username', 'content', 'media', 'created_at').order_by('-created_at')
-    return render(request, "homeApp/base.html/", {'user': user, 'posts': posts})
+    posts = Post.objects.values('id', 'author__username', 'content', 'media', 'created_at', 'likes_count').order_by('-created_at')
+    liked_post_ids = user.liked_posts.values_list('id', flat=True) if user.is_authenticated else []
+
+    return render(request, "homeApp/base.html", {'user': user, 'posts': posts, 'liked_post_ids': liked_post_ids})
 
 
 def create_page(request):
@@ -29,19 +31,18 @@ def create_page(request):
 
 # @login_required
 def like_post(request, post_id):
-    #logged_user = request.user
-    print("nitin")
-    # post = Post.objects.get(id=post_id)
+    logged_user = request.user
+    post = Post.objects.get(id=post_id)
 
-    # if logged_user in post.liked_by.all():
+    if logged_user in post.liked_by.all():
 
-    #     post.likes_count -= 1
-    #     post.liked_by.remove(logged_user)
-    # else:
+        post.likes_count -= 1
+        post.liked_by.remove(logged_user)
+    else:
 
-    #     post.likes_count += 1
-    #     post.liked_by.add(logged_user)
+        post.likes_count += 1
+        post.liked_by.add(logged_user)
 
-    # post.save()
-    # return redirect("/home")
-    return HttpResponse('hisdfok')
+    post.save()
+    return redirect("/home")
+    # return HttpResponse('hisdfok')
