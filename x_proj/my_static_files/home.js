9e0151daefcbbab0897912c7e37a5adcb851e136
Subhadeep Mandal <ultrasubha@gmail.com>
2024-03-06 11:35:53 +0530

    test

--- x_proj/my_static_files/home.js
+++ x_proj/my_static_files/home.js
@@ -1,26 +1,26 @@
-function modifyUrl() {
-  let sectionRoutes = [
-    "#tweets",
-    "#Explore",
-    "#Notifications",
-    "#Messages",
-    "#Grok",
-    "#Lists",
-    "#Communities",
-    "#Premium",
-    "#Profile",
-    "#More",
-    "#Logout",
-  ];
-  let currentUrl = window.location.href;
-  let urlArr = currentUrl.split("/");
-  let section = urlArr[urlArr.length - 2];
-  let trailing = urlArr[urlArr.length - 1];
+// function modifyUrl() {
+//   let sectionRoutes = [
+//     "#tweets",
+//     "#Explore",
+//     "#Notifications",
+//     "#Messages",
+//     "#Grok",
+//     "#Lists",
+//     "#Communities",
+//     "#Premium",
+//     "#Profile",
+//     "#More",
+//     "#Logout",
+//   ];
+//   let currentUrl = window.location.href;
+//   let urlArr = currentUrl.split("/");
+//   let section = urlArr[urlArr.length - 2];
+//   let trailing = urlArr[urlArr.length - 1];
 
-  if (((trailing[0] == "#") && (!sectionRoutes.includes(trailing))) || (section === "home" && trailing == "") ) {
-    location.href = "http://localhost:8000/home/#tweets"
-  }
-}
+//   if (((trailing[0] == "#") && (!sectionRoutes.includes(trailing))) || (section === "home" && trailing == "") ) {
+//     location.href = "http://localhost:8000/home/#tweets"
+//   }
+// }
 
 function toggleLogOut() {
   var logOutDiv = document.querySelector('.logOutSection');
