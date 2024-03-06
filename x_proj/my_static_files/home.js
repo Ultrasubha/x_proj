1bbca46baea444ce8f7da5e6a7b4adac8d4baeb6
Subhadeep Mandal <ultrasubha@gmail.com>
2024-03-06 19:32:01 +0530

    dhasu styles

--- x_proj/my_static_files/home.js
+++ x_proj/my_static_files/home.js
@@ -45,9 +45,11 @@ document.addEventListener('click', function (event) {
 
 document.addEventListener("DOMContentLoaded", function () {
   var inputElement = document.querySelector(".postHere");
+  var postLabelsElement = document.querySelector(".postLabels");
 
   inputElement.addEventListener("click", function () {
     document.getElementById("whoCanReply").style.display = "block";
+    postLabelsElement.style.borderTop = "1px solid rgb(64, 64, 64)";
   });
 });
 
@@ -74,7 +76,8 @@ function displayMedia() {
       }
 
       if (mediaElement) {
-        mediaElement.style.maxWidth = "100%";
+        mediaElement.style.width = "100%";
+        mediaElement.style.height = "auto";
         mediaElement.style.borderRadius = "10px"; // Adjust the radius as needed
         mediaElement.style.maxHeight = "50vh"; // Display only 50% of the height
         mediaElement.style.objectFit = "cover"; // Ensure aspect ratio is maintained
@@ -83,9 +86,52 @@ function displayMedia() {
         mediaContainer.innerHTML = "";
         mediaContainer.appendChild(mediaElement);
         mediaContainer.style.display = "block";
+
+        // Create edit button
+        var editButton = document.createElement("button");
+        editButton.innerText = "Edit";
+        applyButtonStyles(editButton);
+        editButton.style.left = "10px";
+        editButton.addEventListener("click", function () {
+          // Add your edit functionality here
+          console.log("Edit button clicked");
+        });
+        mediaContainer.appendChild(editButton);
+
+        // Create close button
+        var closeButton = document.createElement("button");
+        closeButton.innerHTML = '<i class="fa fa-times"></i>';
+        applyButtonStyles(closeButton);
+        closeButton.style.right = "10px";
+        closeButton.addEventListener("click", function () {
+          mediaContainer.style.display = "none";
+        });
+        mediaContainer.appendChild(closeButton);
       }
     };
 
     reader.readAsDataURL(input.files[0]);
   }
 }
+
+function applyButtonStyles(button) {
+  button.style.position = "absolute";
+  button.style.top = "10px";
+  button.style.padding = "0.75rem";
+  button.style.width = "fit-content";
+  button.style.border = "none";
+  button.style.borderRadius = "10rem";
+  button.style.cursor = "pointer";
+  button.style.transition = "background-color 0.3s ease";
+  button.style.backgroundColor = "rgba(10, 10, 10, 0.7)";
+  button.style.color = "#fff";
+
+  button.addEventListener("mouseover", function () {
+    button.style.backgroundColor = "rgba(60, 60, 60, 0.4)";
+  });
+
+  button.addEventListener("mouseout", function () {
+    button.style.backgroundColor = "rgba(32, 32, 32, 0.7)";
+  });
+}
+
